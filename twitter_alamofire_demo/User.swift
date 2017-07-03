8acb9756220592e5e78db885a19802dc1bae0f93
Elan Halpern <elanh@fb.com>
2017-07-03 16:20:24 -0700

    Added profile picture, and username as attributes of the user class.

--- twitter_alamofire_demo/User.swift
+++ twitter_alamofire_demo/User.swift
@@ -11,9 +11,14 @@ import Foundation
 class User {
     
     var name: String
+    var screenName: String
+    var profileImageUrl: URL?
+    
+    static var current: User?
     
     init(dictionary: [String: Any]) {
         name = dictionary["name"] as! String
-
+        screenName = dictionary["screen_name"] as! String
+        profileImageUrl = dictionary["profile_image_url_https"] as? URL
     }
 }
