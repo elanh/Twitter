87545e1dd70d0658f6c93953c1a588501d8d4b62
Elan Halpern <elanh@fb.com>
2017-07-06 15:49:05 -0700

    -Finalized the detail view controller so thta the likes and retweets update

--- twitter_alamofire_demo/DetailViewController.swift
+++ twitter_alamofire_demo/DetailViewController.swift
@@ -44,10 +44,7 @@ class DetailViewController: UIViewController {
         nameLabel.text = tweet.user.name
         timestampLabel.text = tweet.createdAtString
         
-        let numLikes = String(describing: tweet.favoriteCount)
-        
-        let numRetweet = String(describing: tweet.retweetCount)
-        numLikeAndRetweetLabel.text = "\(numRetweet) Retweet(s) \(numLikes) Like(s)"
+        updateNumLikesAndTweetsLabel()
         
         profilePictureImageView.af_setImage(withURL: tweet.user.profileImageUrl)
         
@@ -63,6 +60,54 @@ class DetailViewController: UIViewController {
             retweetButton.isSelected = false
         }
     }
+    
+    @IBAction func onFavoriteButton(_ sender: Any) {
+        if tweet.favorited! {
+            tweet.favorited = false
+            favoriteButton.isSelected = false
+            tweet.favoriteCount -= 1
+            APIManager.shared.unfavoriteTweet(tweet: tweet) { (tweet, error) in
+            }
+        } else {
+            tweet.favorited = true
+            favoriteButton.isSelected = true
+            tweet.favoriteCount += 1
+            APIManager.shared.favoriteTweet(tweet) { (tweet,error) in
+            }
+        }
+        updateNumLikesAndTweetsLabel()
+    }
+    
+    @IBAction func onRetweetButton(_ sender: Any) {
+        if tweet.retweeted {
+            tweet.retweeted = false
+            retweetButton.isSelected = false
+            tweet.retweetCount -= 1
+            APIManager.shared.unRetweet(tweet: tweet) { (tweet,error) in
+            }
+        } else {
+            tweet.retweeted = true
+            retweetButton.isSelected = true
+            tweet.retweetCount += 1
+            APIManager.shared.retweet(tweet: tweet) { (tweet,error) in
+            }
+        }
+        updateNumLikesAndTweetsLabel()
+    }
+    
+    
+    
+    func updateNumLikesAndTweetsLabel() {
+        let numLikes = String(describing: tweet.favoriteCount)
+        
+        let numRetweet = String(describing: tweet.retweetCount)
+        
+        numLikeAndRetweetLabel.text = "\(numRetweet) Retweet(s) \(numLikes) Like(s)"
+    }
+    
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
